8dcaa2c21a470369ec5259c8834166dbdfa285e5
Benoît Chow Pit Wan <benoit.chow.pit.wan@eisti.fr>
2016-10-07 12:13:24 +0200

    modification des noms des scripts

---
+++ bdd/create_tables.sql
@@ -0,0 +1,33 @@
+-----------------------------------
+-- Script de création des tables --
+-----------------------------------
+-- A exécuter avec le schéma oifyi
+
+
+CREATE OR REPLACE TABLE consultant(
+
+);
+
+CREATE OR REPLACE TABLE absence(
+
+);
+
+CREATE OR REPLACE TABLE cra(
+
+);
+
+CREATE OR REPLACE TABLE notedefrais(
+
+);
+
+CREATE OR REPLACE TABLE client(
+
+);
+
+CREATE OR REPLACE TABLE mission(
+
+);
+
+CREATE OR REPLACE TABLE parametrage(
+	
+);
