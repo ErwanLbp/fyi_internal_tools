cc43772ccc65385817fb96570736e6ab84487929
Erwan_Lbp <erwan.lbp@gmail.com>
2016-10-18 17:36:04 +0200

    Ajoute les fichiers de connexion au site

---
+++ dev/oifyi/src/main/java/dao/ClientDAO.java
@@ -0,0 +1,13 @@
+package dao;
+
+/**
+ * <h1>dao ClientDAO</h1>
+ * TODO Description
+ *
+ * @author Erwan
+ * @version 1.0
+ * @since 18-10-2016
+ */
+public class ClientDAO {
+
+}
---
+++ dev/oifyi/src/main/java/servlets/Connexion.java
@@ -0,0 +1,13 @@
+package servlets;
+
+/**
+ * <h1>servlets Connexion</h1>
+ * TODO Description
+ *
+ * @author Erwan
+ * @version 1.0
+ * @since 18-10-2016
+ */
+public class Connexion {
+
+}
