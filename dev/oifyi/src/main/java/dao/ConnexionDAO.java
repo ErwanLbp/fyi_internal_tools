d73e3653b039352a02d57eb44e664a04242846a6
Erwan_Lbp <erwan.lbp@gmail.com>
2016-10-19 16:32:02 +0200

    Ajoute le traitement du formulaire de connexion

---
+++ dev/oifyi/src/main/java/dao/ConnexionDAO.java
@@ -0,0 +1,16 @@
+package dao;
+
+/**
+ * <h1>dao ConnexionDAO</h1>
+ * TODO Description
+ *
+ * @author Erwan
+ * @version 1.0
+ * @since 19-10-2016
+ */
+public class ConnexionDAO {
+    public static boolean checkLoginPassword(String login, String password) {
+        //TODO checkLoginPassword
+        return false;
+    }
+}
--- dev/oifyi/src/main/java/servlets/Connexion.java
+++ dev/oifyi/src/main/java/servlets/Connexion.java
@@ -1,5 +1,15 @@
 package servlets;
 
+import dao.ConnexionDAO;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * <h1>servlets Connexion</h1>
  * TODO Description
@@ -8,6 +18,31 @@ package servlets;
  * @version 1.0
  * @since 18-10-2016
  */
-public class Connexion {
+public class Connexion extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        String url = getServletContext().getContextPath();
+        boolean valide = true;
+
+        String password = req.getParameter("password");
+        if (password.equals("")) {
+            valide = false;
+            req.setAttribute("erreur", "Le password n'est pas rempli");
+        }
+
+        String login = req.getParameter("login");
+        if (login.equals("")) {
+            valide = false;
+            req.setAttribute("erreur", "Le login n'est pas rempli");
+        }
+
+        if (valide && ConnexionDAO.checkLoginPassword(login, password)) {
+            resp.sendRedirect(url + "?page=accueil");
+        } else {
+            getServletContext().getRequestDispatcher(url).forward(req, resp);
+        }
+
+        resp.sendRedirect(url);
+    }
 }
