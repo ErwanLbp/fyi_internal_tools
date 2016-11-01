022fe0c1594836150ea78f4c0816f8c6ce8435c6
Croute <nandillona@eisti.eu>
2016-11-01 15:25:56 +0100

    modification inserts mapping et droits_pages pour une meilleure base

--- dev/oifyi/src/main/resources/db/migration/oracle/sql/V2016.10.23_17.37__inserts_mapping_url_fichier.sql
+++ dev/oifyi/src/main/resources/db/migration/oracle/sql/V2016.10.23_17.37__inserts_mapping_url_fichier.sql
@@ -1,18 +1,37 @@
 INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('profil', 'connexion', 'WEB-INF/profil/connexion.jsp');
-INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('accueil', 'view', 'WEB-INF/accueil/accueil.jsp');
-
-INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('administration', 'list_pages', 'WEB-INF/administration/list_url.jsp');
-INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('administration', 'parametrage', 'WEB-INF/administration/parametrage.jsp');
-INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('clients', 'list', 'WEB-INF/clients/listing_client.jsp');
-INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('consultant', 'update', 'WEB-INF/consultant/update_consultant.jsp');
-
 INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('profil', 'update', 'WEB-INF/profil/modifier.jsp');
-
 INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('profil', 'view', 'WEB-INF/profil/view.jsp');
 INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('profil', 'deconnexion', 'WEB-INF/profil/deconnexion.jsp');
-
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('accueil', 'view', 'WEB-INF/accueil/accueil.jsp');
 INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('development', 'view', 'WEB-INF/development/view.jsp');
-
-INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('cra', 'view', 'WEB-INF/cra/view.jsp');
-
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('administration', 'list_pages', 'WEB-INF/administration/list_url.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('administration', 'parametrage', 'WEB-INF/administration/parametrage.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('administration', 'droits_roles', 'WEB-INF/administration/droits_roles.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('administration', 'alertes_mails', 'WEB-INF/administration/alertes_mails.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('administration', 'logs', 'WEB-INF/administration/logs_et_audit.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('clients', 'list', 'WEB-INF/clients/listing_client.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('clients', 'update', 'WEB-INF/clients/update_client.jsp');
 INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('clients', 'new', 'WEB-INF/clients/saisie_clients.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('clients', 'facturation', 'WEB-INF/clients/facturation.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('clients', 'facturation_suivi', 'WEB-INF/clients/facturation_suivi.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('consultant', 'list', 'WEB-INF/consultant/listing_consultants.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('consultant', 'update', 'WEB-INF/consultant/update_consultant.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('consultant', 'paye_suivi', 'WEB-INF/consultant/suivi_paye.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('cra', 'view', 'WEB-INF/cra/view.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('cra', 'saisie', 'WEB-INF/consultant/saisie_cra.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('cra', 'validation', 'WEB-INF/consultant/validation_cra.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('absences', 'saisie', 'WEB-INF/consultant/saisie_absences.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('absences', 'validation', 'WEB-INF/consultant/validation_absences.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('absences', 'update', 'WEB-INF/consultant/update_absence.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('missions', 'list', 'WEB-INF/missions/listing_missions.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('missions', 'facturation', 'WEB-INF/missions/saisie_facturation.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('missions', 'facturation_suivi', 'WEB-INF/missions/suivi_facturation.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('missions', 'update', 'WEB-INF/missions/update_mission.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('mission_consultant', 'list', 'WEB-INF/missions/listing_mission_consultant.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('notes_de_frais', 'saisie', 'WEB-INF/consultant/saisie_notes_de_frais.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('notes_de_frais', 'list', 'WEB-INF/consultant/list_note_frais.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('notes_de_frais', 'validation', 'WEB-INF/consultant/valid_note_frais.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('fournisseurs', 'facturation', 'WEB-INF/clients/factu_fournisseurs.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('fournisseurs', 'contrats', 'WEB-INF/clients/contrats_fournisseurs.jsp');
+INSERT INTO mapping_url_fichier (nom_page, nom_mode, chemin_fichier) VALUES ('independants', 'facturation', 'WEB-INF/consultant/facturation_indep.jsp');
+
