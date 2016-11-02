fb97493ec3c1bd6c63404d84d92708062cdd2e83
Croute <nandillona@eisti.eu>
2016-11-02 15:41:28 +0100

    Modif BDD mission_consultant => suppression du champ PRIX

--- dev/oifyi/src/main/resources/db/migration/oracle/sql/V2016.10.21_12.05__mission_consultant.sql
+++ dev/oifyi/src/main/resources/db/migration/oracle/sql/V2016.10.21_12.05__mission_consultant.sql
@@ -1,7 +1,6 @@
 CREATE TABLE mission_consultant (
   consultant_id INTEGER NOT NULL,
   mission_id    INTEGER NOT NULL,
-  prix          INTEGER,
   CONSTRAINT mission_consultant_PK PRIMARY KEY (consultant_id, mission_id),
   CONSTRAINT mission_consult_consult_FK FOREIGN KEY (consultant_id) REFERENCES consultant (id_consultant),
   CONSTRAINT mission_consultant_mission_FK FOREIGN KEY (mission_id) REFERENCES mission (id_mission)
